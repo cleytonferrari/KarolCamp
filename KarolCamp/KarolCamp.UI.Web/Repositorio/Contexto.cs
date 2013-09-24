e761be8eccec263485a4f5bb97e47c38ea50b025
Cleyton Ferrari <cleyton@w7br.com>
2013-09-23 20:35:45 -0400

    adicionado repositorio do mongoDb

---
+++ KarolCamp/KarolCamp.UI.Web/Repositorio/Contexto.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Web;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization;
+using MongoDB.Bson.Serialization.Conventions;
+using MongoDB.Bson.Serialization.Options;
+using MongoDB.Driver;
+using MongoDB.Driver.Builders;
+using MongoDB.Driver.GridFS;
+
+namespace KarolCamp.UI.Web.Repositorio
+{
+    public class Contexto<T>
+    {
+        private readonly MongoDatabase database;
+
+        private static string GetMongoDbConnectionString()
+        {
+            return ConfigurationManager.AppSettings.Get("MONGOLAB_URI") ??
+                   ConfigurationManager.ConnectionStrings["KarolCamp"].ConnectionString;
+        }
+
+        public Contexto()
+        {
+            var url = new MongoUrl(GetMongoDbConnectionString());
+            var client = new MongoClient(url);
+            var server = client.GetServer();
+            database = server.GetDatabase(url.DatabaseName);
+            Collection = database.GetCollection<T>(typeof(T).Name.ToLower());
+            DateTimeSerializationOptions.Defaults = new DateTimeSerializationOptions(DateTimeKind.Local, BsonType.Document);
+            //Ajuda no migration, se tiver campo a mais no banco ele ignora
+            var convensoes = new ConventionProfile();
+            convensoes.SetIgnoreExtraElementsConvention(new AlwaysIgnoreExtraElementsConvention());
+            BsonClassMap.RegisterConventions(convensoes, (type) => true);
+        }
+
+        public MongoCollection<T> Collection { get; private set; }
+
+        public Dictionary<string, string> BuscarArquivo(string id, ref MemoryStream retorno)
+        {
+            var fileInfo = database.GridFS.FindOne(Query.EQ("_id", new BsonObjectId(id)));
+            var gfs = new MongoGridFS(database);
+            gfs.Download(retorno, fileInfo);
+            return new Dictionary<string, string> { { fileInfo.ContentType, fileInfo.Name } };
+        }
+
+        public void ExcluirArquivo(string id)
+        {
+            var gfs = new MongoGridFS(database);
+            gfs.Delete(Query.EQ("_id", id));
+        }
+
+        public string InserirArquivo(Stream arquivo, string nome, string contentType)
+        {
+            var gfs = new MongoGridFS(database);
+            var fileInfo = gfs.Upload(arquivo, nome);
+            gfs.SetContentType(fileInfo, contentType);
+            return fileInfo.Id.AsObjectId.ToString();
+        }
+    }
+}
