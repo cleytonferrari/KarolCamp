258440547287b420e4b194855bac01f5e7bd3b4c
Cleyton Ferrari <cleyton@w7br.com>
2013-09-24 20:21:32 -0400

    adicionado o cadastro de palestrante, sem foto

---
+++ KarolCamp/KarolCamp.UI.Web/Aplicacao/PalestranteAplicacao.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using KarolCamp.UI.Web.Models;
+using KarolCamp.UI.Web.Repositorio;
+using MongoDB.Bson;
+using MongoDB.Driver.Builders;
+using MongoDB.Driver.Linq;
+
+namespace KarolCamp.UI.Web.Aplicacao
+{
+    public class PalestranteAplicacao
+    {
+        private readonly Contexto<Palestrante> contexto;
+
+        public PalestranteAplicacao()
+        {
+            contexto = new Contexto<Palestrante>();
+        }
+
+        public void Salvar(Palestrante palestrante)
+        {
+            contexto.Collection.Save(palestrante);
+        }
+
+        public void Excluir(string id)
+        {
+            contexto.Collection.Remove(Query.EQ("_id", id));
+        }
+
+        public IEnumerable<Palestrante> ListarTodos()
+        {
+            return contexto.Collection.AsQueryable();
+        }
+
+        public Palestrante ListarPorId(string id)
+        {
+            return contexto.Collection.AsQueryable().FirstOrDefault(x => x.Id == id);
+        }
+
+    }
+}
---
+++ KarolCamp/KarolCamp.UI.Web/Controllers/PalestranteController.cs
@@ -0,0 +1,88 @@
+﻿using System.Web.Mvc;
+using KarolCamp.UI.Web.Aplicacao;
+using KarolCamp.UI.Web.Models;
+
+namespace KarolCamp.UI.Web.Controllers
+{
+    public class PalestranteController : Controller
+    {
+        //
+        // GET: /Palestrante/
+
+        public ActionResult Index()
+        {
+            var app = new PalestranteAplicacao();
+            return View(app.ListarTodos());
+        }
+
+        public ActionResult Cadastrar()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Cadastrar(Palestrante palestrante)
+        {
+            if (ModelState.IsValid)
+            {
+                var app = new PalestranteAplicacao();
+                app.Salvar(palestrante);
+                return RedirectToAction("Index");
+            }
+            return View(palestrante);
+        }
+
+        public ActionResult Editar(string id)
+        {
+            var app = new PalestranteAplicacao();
+            var palestrante = app.ListarPorId(id);
+            if (palestrante == null)
+                return HttpNotFound();
+
+            return View(palestrante);
+        }
+
+        [HttpPost]
+        public ActionResult Editar(Palestrante palestrante)
+        {
+            if (ModelState.IsValid)
+            {
+                var app = new PalestranteAplicacao();
+                app.Salvar(palestrante);
+                return RedirectToAction("Index");
+            }
+            return View(palestrante);
+
+        }
+
+        public ActionResult Deletar(string id)
+        {
+            var app = new PalestranteAplicacao();
+            var palestrante = app.ListarPorId(id);
+            if (palestrante == null)
+                return HttpNotFound();
+
+            return View(palestrante);
+        }
+
+        [HttpPost, ActionName("Deletar")]
+        public ActionResult ConfirmaDeletar(string id)
+        {
+            //Todo: Não posso excluir palestrante se estiver utilizando na palestra
+            var app = new PalestranteAplicacao();
+            app.Excluir(id);
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Detalhe(string id)
+        {
+            var app = new PalestranteAplicacao();
+            var palestrante = app.ListarPorId(id);
+            if (palestrante == null)
+                return HttpNotFound();
+
+            return View(palestrante);
+        }
+
+    }
+}
