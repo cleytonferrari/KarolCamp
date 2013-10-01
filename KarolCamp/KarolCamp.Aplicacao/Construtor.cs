b446961e7a2da893687eebe6d02a604f01ba5ec2
Cleyton Ferrari <cleyton@w7br.com>
2013-09-30 21:43:14 -0400

    correcao do construtor

--- KarolCamp/KarolCamp.Aplicacao/Construtor.cs
+++ KarolCamp/KarolCamp.Aplicacao/Construtor.cs
@@ -1,32 +1,44 @@
-﻿
-
-using KarolCamp.Repositorio.Mongo;
-
-namespace KarolCamp.Aplicacao
+﻿namespace KarolCamp.Aplicacao
 {
     public class Construtor
     {
         public static PalestraAplicacao PalestraAplicacaoMongo()
         {
-            //return new PalestraAplicacao(new RepositorioPalestra());
+            return new PalestraAplicacao(new Repositorio.Mongo.RepositorioPalestra());
+        }
+
+        public static PalestraAplicacao PalestraAplicacaoEF()
+        {
             return new PalestraAplicacao(new Repositorio.EF.RepositorioPalestra());
         }
 
         public static PalestranteAplicacao PalestranteAplicacaoMongo()
         {
-            //return new PalestranteAplicacao(new RepositorioPalestrante(), new RepositorioArquivo());
+            return new PalestranteAplicacao(new Repositorio.Mongo.RepositorioPalestrante(), new Repositorio.Mongo.RepositorioArquivo());
+        }
+
+        public static PalestranteAplicacao PalestranteAplicacaoEF()
+        {
             return new PalestranteAplicacao(new Repositorio.EF.RepositorioPalestrante(), new Repositorio.EF.RepositorioArquivo());
         }
 
         public static SalaAplicacao SalaAplicacaoMongo()
         {
-            //return new SalaAplicacao(new RepositorioSala());
+            return new SalaAplicacao(new Repositorio.Mongo.RepositorioSala());
+        }
+
+        public static SalaAplicacao SalaAplicacaoEF()
+        {
             return new SalaAplicacao(new Repositorio.EF.RepositorioSala());
         }
 
         public static TrilhaAplicacao TrilhaAplicacaoMongo()
         {
-            //return new TrilhaAplicacao(new RepositorioTrilha());
+            return new TrilhaAplicacao(new Repositorio.Mongo.RepositorioTrilha());
+        }
+
+        public static TrilhaAplicacao TrilhaAplicacaoEF()
+        {
             return new TrilhaAplicacao(new Repositorio.EF.RepositorioTrilha());
         }
     }
