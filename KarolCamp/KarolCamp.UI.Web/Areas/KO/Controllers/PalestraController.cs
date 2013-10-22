6b1b719f458b4402c6468e0108cc3b5fd861dfaa
Cleyto Ferrari <cleyton@w7br.com>
2013-10-21 21:48:28 -0400

    adicionado area para separar o codigo KO

---
+++ KarolCamp/KarolCamp.UI.Web/Areas/KO/Controllers/PalestraController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace KarolCamp.UI.Web.Areas.KO.Controllers
+{
+    public class PalestraController : Controller
+    {
+        //
+        // GET: /KO/Palestra/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ KarolCamp/KarolCamp.UI.Web/Areas/KO/Controllers/SalaController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace KarolCamp.UI.Web.Areas.KO.Controllers
+{
+    public class SalaController : Controller
+    {
+        //
+        // GET: /KO/Sala/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
