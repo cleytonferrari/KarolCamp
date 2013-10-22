0bc1dedcee5dd3d467fcb5f6a8f68de01389bb66
Cleyto Ferrari <cleyton@w7br.com>
2013-10-21 22:27:27 -0400

    adicionado API, pagina home feita em KO

---
+++ KarolCamp/KarolCamp.UI.Web/Api/PalestrantesController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using KarolCamp.Aplicacao;
+using KarolCamp.Dominio;
+
+namespace KarolCamp.UI.Web.Api
+{
+    public class PalestrantesController : ApiController
+    {
+        // GET api/palestrantes
+        public IEnumerable<Palestrante> Get()
+        {
+            return Construtor.PalestranteAplicacaoMongo().ListarTodos().ToList();
+        }
+
+        // GET api/palestrantes/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/palestrantes
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/palestrantes/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/palestrantes/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
--- KarolCamp/KarolCamp.UI.Web/App_Start/RouteConfig.cs
+++ KarolCamp/KarolCamp.UI.Web/App_Start/RouteConfig.cs
@@ -16,7 +16,8 @@ namespace KarolCamp.UI.Web
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional },
+                namespaces: new[] { "KarolCamp.UI.Web.Controllers" }
             );
         }
     }
