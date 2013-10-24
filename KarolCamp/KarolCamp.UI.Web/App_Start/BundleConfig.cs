fde779100b82b3f9abacd2a5309f03ab85f20c71
Cleyto Ferrari <cleyton@w7br.com>
2013-10-23 22:35:08 -0400

    adicionado projeto SPA, criado pagina inicial e lista de trilha

--- KarolCamp/KarolCamp.UI.Web/App_Start/BundleConfig.cs
+++ KarolCamp/KarolCamp.UI.Web/App_Start/BundleConfig.cs
@@ -8,11 +8,18 @@ namespace KarolCamp.UI.Web
         // For more information on Bundling, visit http://go.microsoft.com/fwlink/?LinkId=254725
         public static void RegisterBundles(BundleCollection bundles)
         {
+            BundleTable.EnableOptimizations = true;
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/knockout").Include(
                         "~/Scripts/knockout-{version}.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/require").Include(
+                        "~/Scripts/require.js"
+                       ));
+            bundles.Add(new ScriptBundle("~/bundles/sammy").Include(
+                "~/Scripts/sammy-{version}.js"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
---
+++ KarolCamp/KarolCamp.UI.Web/Areas/SPA/SPAAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace KarolCamp.UI.Web.Areas.SPA
+{
+    public class SPAAreaRegistration : AreaRegistration
+    {
+        public override string AreaName
+        {
+            get
+            {
+                return "SPA";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context)
+        {
+            context.MapRoute(
+                "SPA_default",
+                "SPA/{controller}/{action}/{id}",
+                new { action = "Index", controller="Home", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
